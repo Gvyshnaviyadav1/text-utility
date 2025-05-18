859a5b7c09667cc314d38220a3e581a5120c1491
Gvyshnaviyadav1 <vyshnavi.yadav.23031@iitgoa.ac.in>
2025-05-18 16:43:36 +0530

    nice

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/textform';
+import About from './components/About';
 
 function App() {
   return (
     <>
    
-<Navbar/>
+<Navbar title="TextReframe"/>
+<div className='container my-3' ><TextForm heading="Enter the required text"/></div>
+<About/>
    </>
+   
   );
 }
 
---
+++ src/components/textform.js
@@ -0,0 +1,51 @@
+import React,{useState} from 'react';
+
+
+export default function TextForm(props) {
+     const handleOnChange=(event)=>{
+        setText(event.target.value)
+     }
+     const handleUpClick=()=>{
+        let newText=text.toUpperCase();
+        setText(newText)
+     }
+     const handleLoClick=()=>{
+        let newText=text.toLowerCase();
+        setText(newText)
+     }
+     const copyText=()=>{
+        var text=document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+     }
+      const clearSpace=()=>{
+        let newText=text.split(/[ ]+/);
+        setText(newText.join(" "))
+     }
+    const [text, setText]=useState("enter the text");
+  return (
+    <>
+    
+      <div className='conatainer'>
+        <h1>{props.heading}</h1>
+        
+        <textarea className='form-control' id="myBox" rows="8" value={text} onChange={handleOnChange}></textarea>
+         <button className="btn btn-primary my-3" onClick={handleUpClick}>Convert to upper case</button>
+      <button className="btn btn-primary mx-3 my-3" onClick={handleLoClick}>Convert to lower case</button>
+      <button className="btn btn-primary  my-3" onClick={copyText}>Copy text</button>
+      <button className="btn btn-primary mx-3 my-3" onClick={clearSpace}>Clear Space</button>
+      </div>
+      <div className='container'>
+        <h1>Text Summary</h1>
+        <p>{text.split(" ").length} words and {text.length} characters </p>
+         <h2>Preview</h2>
+         <p>{text}</p>
+      </div>
+      
+
+     
+
+      
+    </>
+  );
+}
