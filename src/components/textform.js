bd1ddd9108020473693f7cb67def3297b2c28670
Gvyshnaviyadav1 <vyshnavi.yadav.23031@iitgoa.ac.in>
2025-05-19 17:18:44 +0530

    com

--- src/components/textform.js
+++ src/components/textform.js
@@ -26,16 +26,16 @@ export default function TextForm(props) {
   return (
     <>
     
-      <div className='conatainer'>
+      <div className='conatainer' style={{color:props.mode==='dark'?'white':'black'}}>
         <h1>{props.heading}</h1>
         
-        <textarea className='form-control' id="myBox" rows="8" value={text} onChange={handleOnChange}style={{backgroundColor:props.mode==='light'?'dark':'light'}}></textarea>
+        <textarea className='form-control' id="myBox" rows="8" value={text} onChange={handleOnChange}style={{backgroundColor:props.mode==='dark'?'grey':'white' ,color:props.mode==='dark'?'white':'black'}}></textarea>
          <button className="btn btn-primary my-3" onClick={handleUpClick}>Convert to upper case</button>
       <button className="btn btn-primary mx-3 my-3" onClick={handleLoClick}>Convert to lower case</button>
       <button className="btn btn-primary  my-3" onClick={copyText}>Copy text</button>
       <button className="btn btn-primary mx-3 my-3" onClick={clearSpace}>Clear Space</button>
       </div>
-      <div className='container'>
+      <div className='container' style={{color:props.mode==='dark'?'white':'black'}}>
         <h1>Text Summary</h1>
         <p>{text.split(" ").length} words and {text.length} characters </p>
          <h2>Preview</h2>
